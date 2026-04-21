f0ef1f0d7026581722752f75109359383d501ecc
Rohith Marri <rmarri@my.bridgeport.edu>
2026-04-21 17:22:21 -0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,48 @@
+class MinStack {
+
+    Stack<int[]> stack;
+    int min;
+
+    public MinStack() {
+
+        stack = new Stack<>();
+
+         
+    }
+    
+    public void push(int val) {
+        
+        if(stack.isEmpty())
+        {
+            min = val;
+            stack.push(new int[]{val,min});
+        }
+        else 
+        {
+            if(min > val)
+            {
+                min = val;
+            }
+            stack.push(new int[]{val,min});
+        }
+        
+    }
+    
+    public void pop() {
+
+        stack.pop();
+        
+    }
+    
+    public int top() {
+
+        return stack.peek()[0];
+        
+    }
+    
+    public int getMin() {
+
+        return stack.peek()[1];
+        
+    }
+}
