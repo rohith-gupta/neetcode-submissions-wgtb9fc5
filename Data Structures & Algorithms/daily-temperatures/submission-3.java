0526b646df2f3ac1b3a3800690dd37448c31e52a
Rohith Marri <rmarri@my.bridgeport.edu>
2026-04-22 15:11:51 -0500

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        int [] result = new int[temp.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=temp.length-1;i>=0;i--)
+        {
+            while(!stack.isEmpty())
+            {
+                if(temp[i] >= temp[stack.peek()])
+                {
+                    stack.pop();
+                }
+                else {
+                    result[i] = stack.peek() - i;
+                    stack.push(i);
+                    break;
+                }
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
